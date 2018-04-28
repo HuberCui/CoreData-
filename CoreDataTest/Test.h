6a25277100c056fc80124ba19ad29b98a7617de6
Guest <836138170@qq.com>
2018-04-28 09:37:15 +0800

    test

---
+++ CoreDataTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  CoreDataTest
+//
+//  Created by Guest on 2018/4/28.
+//  Copyright © 2018年 Xuebin Cui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ CoreDataTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  CoreDataTest
+//
+//  Created by Guest on 2018/4/28.
+//  Copyright © 2018年 Xuebin Cui. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
