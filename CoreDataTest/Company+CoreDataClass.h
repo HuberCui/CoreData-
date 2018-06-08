1775ce793fd84101a33190b1134046af63f122e8
Guest <836138170@qq.com>
2018-06-08 17:13:12 +0800

    chongtu

--- CoreDataTest/Company+CoreDataClass.h
+++ CoreDataTest/Company+CoreDataClass.h
@@ -5,11 +5,9 @@
 //  Created by Xuebin Cui on 2018/4/9.
 //  Copyright © 2018年 Xuebin Cui. All rights reserved.
 //
-<<<<<<< HEAD
-//afnetworking
-=======
+
 //AAAA周五
->>>>>>> 23ca0dd6abb281fc31af93ae562b7bb1548567e4
+
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
